af42f246dd66f13c8a42fa964ecf44071701ee8f
Shekhar34 <shekhargite6123@gmail.com>
2024-03-05 11:03:57 +0530

    authtoken json webtoken

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express= require('express');
 const User = require('../models/User');
 const router=express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET="shekhar$goodbody";
 
 router.post('/createuser',[
     body('name','enter valid name').isLength({ min: 5 }),
@@ -16,11 +20,22 @@ router.post('/createuser',[
     if(user){
       return res.status(400).json({error: "eamil with same eamil found"});
     }
+    const salt =await bcrypt.genSaltSync(10);
+    const secPass=await bcrypt.hash(req.body.password, salt);
+
      user=await  User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
-      })
+        password:secPass,
+      });
+
+      const data={
+        user:{
+          id:user.id
+        }
+      }
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      res.json({authtoken});
 //       .then(user => res.json(user)).catch(err=>console.log(err));
  })
 
